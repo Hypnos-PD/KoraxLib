b2d9f45b8b245db10c34f410a80438d9fe680366
Aspharos <ves.hypnos@gmail.com>
2026-07-07 08:08:20 +0800

    feat: 更新开发环境配置，添加 actionlint、nodejs 和 playwright-driver 作为构建输入

--- flake.nix
+++ flake.nix
@@ -8,11 +8,16 @@
   in {
     devShells.${system}.default = pkgs.mkShell {
       buildInputs = [
+        pkgs.actionlint
         pkgs.dotnet-sdk_9
+        pkgs.nodejs_22
+        pkgs.playwright-driver
       ];
 
       shellHook = ''
         export DOTNET_ROOT=${pkgs.dotnet-sdk_9}/share/dotnet
+        export PLAYWRIGHT_BROWSERS_PATH=${pkgs.playwright-driver.browsers}
+        export PLAYWRIGHT_SKIP_BROWSER_DOWNLOAD=1
         echo "已进入 KoraxLib 开发环境"
       '';
     };
